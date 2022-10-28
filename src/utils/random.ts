7807a6ffbf3cccc275fe0759f43eb4c15d935dba
Goga Barabadze <goga.barabadze73@gmail.com>
2022-10-28 18:20:17 +0200

    fix: removed inferred type

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,3 +1,3 @@
-export function randomHex(size: number = 8) {
-    return [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('')
+export function randomHex(size = 8) {
+  return [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join("")
 }
